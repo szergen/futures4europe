2d11c7e1ab836a4bb14ef281c460cd983ecf8749
szergen <szergen@gmail.com>
2024-09-21 10:43:05 +0300

    [dashboard-cms-3] Build try 2

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -9,12 +9,12 @@ import {
 
 // Next.js will invalidate the cache when a
 // request comes in, at most once every 120 seconds.
-export const revalidate = 120;
+// export const revalidate = 120;
 
 // We'll prerender only the params from `generateStaticParams` at build time.
 // If a request comes in for a path that hasn't been generated,
 // Next.js will server-render the page on-demand.
-export const dynamicParams = true; // or false, to 404 on unknown paths
+// export const dynamicParams = true; // or false, to 404 on unknown paths
 
 // Function to generate static paths
 // export async function generateStaticParams() {
