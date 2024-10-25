06fbdcbcd3cb05f1cc8bcf0087727ff83b32a48a
Catalin Sirbu <catalinssirbu@gmail.com>
2024-10-25 05:31:57 +0300

    [Style] Enhance InfoPagesImageFileUploader with improved hover effects and updated SVG icon

--- app/shared-components/InfoPagesImageFileUploader/InfoPagesImageFileUploader.tsx
+++ app/shared-components/InfoPagesImageFileUploader/InfoPagesImageFileUploader.tsx
@@ -6,6 +6,7 @@ import { HiInformationCircle } from 'react-icons/hi';
 import { getImageUrlForMedia } from '@app/page-components/PageComponents.utils';
 import classNames from 'classnames';
 import { useAuth } from '@app/custom-hooks/AuthContext/AuthContext';
+import SpriteSvg from '@app/shared-components/SpriteSvg/SpriteSvg';
 import style from './InfoPagesImageFileUploader.module.css';
 // import WixMediaImage from '../WixMediaImage/WixMediaImage';
 
@@ -63,9 +64,15 @@ const InfoPagesImageFileUploader: React.FC<FileUploaderProps> = ({
           <span className="top-10 relative">
             {!imageURL || imageURL === ' ' ? 'Upload Image' : ''}
           </span>
-          <svg width="18" height="18" viewBox="0 0 24 24" focusable="false">
-            <path d="M20.41 4.94l-1.35-1.35c-.78-.78-2.05-.78-2.83 0L3 16.82V21h4.18L20.41 7.77c.79-.78.79-2.05 0-2.83zm-14 14.12L5 19v-1.36l9.82-9.82 1.41 1.41-9.82 9.83z"></path>
-          </svg>
+          <SpriteSvg.AccountEdit
+            className="text-site-black mb-6 text-[var(--color-text-icon-error)]"
+            sizeW={42}
+            sizeH={42}
+            viewBox={'0 0 24 24'}
+            stroke={'var(--color-text-brand-tag)'}
+            strokeWidth={2}
+            inline={false}
+          />
         </div>
 
         <FileInput
