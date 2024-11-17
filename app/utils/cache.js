317cf3a9abeb7f3a495f821dcaf32570f3df3ebe
Sergiu Ciobanasu <szergen@gmail.com>
2024-11-17 20:17:55 +0000

    Tags and pages optimization fix (#76)
    * Added cache to gitignore
    
    * added edge config
    
    * fixed save key

--- app/utils/cache.js
+++ app/utils/cache.js
@@ -1,24 +1,33 @@
 import fs from 'fs';
 import path from 'path';
+import { set, get } from '@vercel/edge-config';
 
 const cacheDir = path.join(process.cwd(), 'cache');
 
-export function saveToCache(filename, data) {
-  if (!fs.existsSync(cacheDir)) {
-    fs.mkdirSync(cacheDir);
+export async function saveToCache(filename, data) {
+  if (process.env.NEXT_PUBLIC_NODE_ENV === 'localhost') {
+    if (!fs.existsSync(cacheDir)) {
+      fs.mkdirSync(cacheDir);
+    }
+    console.log('Saving to cache:', filename);
+    console.log('cacheDir:', cacheDir);
+    fs.writeFileSync(path.join(cacheDir, filename), JSON.stringify(data));
+  } else {
+    await set(filename?.replace('.json', ''), data);
   }
-  console.log('Saving to cache:', filename);
-  console.log('cacheDir:', cacheDir);
-  fs.writeFileSync(path.join(cacheDir, filename), JSON.stringify(data));
 }
 
-export function getFromCache(filename) {
-  const filePath = path.join(cacheDir, filename);
-  if (fs.existsSync(filePath)) {
-    const fileData = fs.readFileSync(filePath, 'utf8');
-    console.log('Getting from cache:', filename);
-    console.log('cacheDir:', cacheDir);
-    return JSON.parse(fileData);
+export async function getFromCache(filename) {
+  if (process.env.NEXT_PUBLIC_NODE_ENV === 'localhost') {
+    const filePath = path.join(cacheDir, filename);
+    if (fs.existsSync(filePath)) {
+      const fileData = fs.readFileSync(filePath, 'utf8');
+      console.log('Getting from cache:', filename);
+      console.log('cacheDir:', cacheDir);
+      return JSON.parse(fileData);
+    }
+    return null;
+  } else {
+    return await get(filename?.replace('.json', ''));
   }
-  return null;
 }
