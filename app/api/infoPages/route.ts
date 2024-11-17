a77b3e91afdb10b237705accf300d290e6590467
Sergiu Ciobanasu <szergen@gmail.com>
2024-11-17 23:59:14 +0200

    Revert "Revert "Tags and pages optimization (#75)" (#78)"
    This reverts commit 85c83879bf6ac09d86addbcb77136f052f5483ad.

---
+++ app/api/infoPages/route.ts
@@ -0,0 +1,87 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getWixClientData } from '@app/hooks/useWixClientServer';
+import { saveToCache, getFromCache } from '../../utils/cache';
+import { referencedItemOptions } from '@app/wixUtils/server-side';
+
+export const GET = async (req: NextRequest) => {
+  const cacheKey = 'infoPages.json';
+  const cachedData = await getFromCache(cacheKey);
+
+  if (cachedData) {
+    return new Response(JSON.stringify(cachedData), { status: 200 });
+  }
+
+  try {
+    const wixClient = await getWixClientData();
+
+    let allItems = [] as any[];
+    let skip = 0;
+    const limit = 1000;
+    let totalCount = 0;
+
+    do {
+      const result = await wixClient.items
+        .queryDataItems({
+          dataCollectionId: 'InfoPages',
+          referencedItemOptions: referencedItemOptions,
+          returnTotalCount: true,
+        })
+        .skip(skip)
+        .limit(limit)
+        .find();
+      allItems = [...allItems, ...result?._items];
+      totalCount = result?._totalCount;
+      skip = limit + skip;
+    } while (skip < totalCount);
+    // console.log('allItems', allItems);
+
+    await saveToCache(cacheKey, allItems);
+    return NextResponse.json(allItems, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching infoPages:', error);
+    return NextResponse.json(
+      { message: 'Error fetching infoPages' },
+      { status: 500 }
+    );
+  }
+};
+
+export const POST = async (req: NextRequest) => {
+  const cacheKey = 'infoPages.json';
+
+  try {
+    const wixClient = await getWixClientData();
+
+    let allItems = [] as any[];
+    let skip = 0;
+    const limit = 1000;
+    let totalCount = 0;
+
+    do {
+      const result = await wixClient.items
+        .queryDataItems({
+          dataCollectionId: 'InfoPages',
+          referencedItemOptions: referencedItemOptions,
+          returnTotalCount: true,
+        })
+        .skip(skip)
+        .limit(limit)
+        .find();
+      allItems = [...allItems, ...result?._items];
+      totalCount = result?._totalCount;
+      skip = limit + skip;
+    } while (skip < totalCount);
+    // console.log('allItems', allItems);
+
+    await saveToCache(cacheKey, allItems);
+    return new Response(
+      JSON.stringify({ message: 'Cache updated successfully.' }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error('Error updating cache:', error);
+    return new Response(JSON.stringify({ error: 'Failed to update cache' }), {
+      status: 500,
+    });
+  }
+};
