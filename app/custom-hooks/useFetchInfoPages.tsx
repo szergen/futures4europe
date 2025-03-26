70beb21cdea5f831a0b20d49d1b481f2c1d9e846
Alexandru-Sergiu Ciobanasu <szergen@gmail.com>
2025-03-26 20:04:49 +0200

    Revert "Data optimization (#145)"
    This reverts commit 32b1703d9b962e12293fcef98ece11bf7538f2d2.

--- app/custom-hooks/useFetchInfoPages.tsx
+++ app/custom-hooks/useFetchInfoPages.tsx
@@ -1,12 +1,38 @@
 import { useEffect, useState } from 'react';
+import { items } from '@wix/data';
+import { useWixModules } from '@wix/sdk-react';
+import { referencedItemOptions } from '@app/wixUtils/server-side';
 
-const useFetchInfoPages = (refresh: boolean) => {
+const useFetchInfoPages = (
+  refresh: boolean
+  // setIsLoadingInProgress: (value: boolean) => void
+) => {
   const [infoPages, setInfoPages] = useState<any[]>([]);
   const [infoPagesFetched, setInfoPagesFetched] = useState(false);
 
+  // const { queryDataItems } = useWixModules(items);
+
   useEffect(() => {
     const fetchInfoPages = async () => {
+      // setIsLoadingInProgress(true);
       try {
+        // let allInfoPages = [] as any[];
+        // let skip = 0;
+        // const limit = 50;
+        // let totalCount = 0;
+        // do {
+        //   const result = await queryDataItems({
+        //     dataCollectionId: 'InfoPages',
+        //     referencedItemOptions: referencedItemOptions,
+        //     returnTotalCount: true,
+        //   })
+        //     .skip(skip)
+        //     .limit(limit)
+        //     .find();
+        //   allInfoPages = [...allInfoPages, ...result?._items];
+        //   totalCount = result?._totalCount;
+        //   skip += limit;
+        // } while (skip < totalCount);
         const allInfoPagesResponse = await fetch('/api/infoPages');
         const allInfoPages = await allInfoPagesResponse.json();
 
@@ -15,6 +41,7 @@ const useFetchInfoPages = (refresh: boolean) => {
       } catch (error) {
         console.error('Error fetching info pages:', error);
       }
+      // setIsLoadingInProgress(false);
     };
 
     fetchInfoPages();
