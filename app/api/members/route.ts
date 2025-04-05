7360c31a10916c2d593c837cfd4b1e4cab6eaafd
Catalin Sirbu <catalinssirbu@gmail.com>
2025-04-05 21:03:33 +0300

    Enhance PostPageComponent layout and functionality; add admin delete confirmation and adjust styles for better responsiveness

---
+++ app/api/members/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getWixClientServerData } from '@app/hooks/useWixClientServer';
+
+export async function GET(req: NextRequest) {
+  try {
+    const wixClient = await getWixClientServerData();
+    
+    let allContacts = [];
+    let skip = 0;
+    const limit = 1000;
+    let totalCount = 0;
+
+    do {
+      const result = await wixClient.contacts
+      .queryContacts()
+      .skip(skip)
+      .limit(limit)
+      .find();  
+      
+      allContacts = [...allContacts, ...result.items];
+      totalCount = result.totalCount || result.items.length; // Fallback in case totalCount is missing
+      skip += limit;
+    } while (skip < totalCount);
+
+    
+    return NextResponse.json({
+      success: true,
+      contacts: allContacts,
+    }, { status: 200 });
+  } catch (error: any) {
+    console.error('Error fetching contacts:', error);
+    return NextResponse.json(
+      { message: 'Error fetching contacts', error: error.message },
+      { status: 500 }
+    );
+  }
+}
