5c51552f55068e9a28c1531143561e79cc8876af
szergen <szergen@gmail.com>
2025-12-05 16:22:45 +0200

    feat: Added December Newsletter

--- app/shared-components/Layout/Footer.tsx
+++ app/shared-components/Layout/Footer.tsx
@@ -186,6 +186,13 @@ const Footer = () => {
                     <li className="mb-2">
                       <Link
                         target="_blank"
+                        href="https://shoutout.wix.com/so/b9PgxhZAB"
+                        className="text-white/80 hover:text-white underline"
+                      >
+                        December 2025
+                      </Link>
+                      <Link
+                        target="_blank"
                         href="https://shoutout.wix.com/so/dbPc0ZTvy"
                         className="text-white/80 hover:text-white underline"
                       >
